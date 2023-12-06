364ff8ce9aa7e55b78bfa283391736fb39866c0d
dyorcat <yool0417@gmail.com>
2023-12-06 12:22:11 +0900

    오타 수정

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,4 +1,4 @@
-class Calculator() {
+class Calculator {
     var num1: Int = 0
     var sym: String = "+"
     var num2: Int = 0
@@ -10,7 +10,7 @@ class Calculator() {
             }
 
             "-" -> {
-                println("${num1} 더하기 ${num2}는 ${SubstractOperation().minus(num1, num2)} 입니다. ")
+                println("${num1} 빼기 ${num2}는 ${SubstractOperation().minus(num1, num2)} 입니다. ")
             }
 
             "*" -> {
@@ -60,8 +60,8 @@ class DivideOperation {
 
 fun main() {
     var calculator = Calculator()
-    println("계산기")
-    println("첫 번째 숫자를 입력하세요: ")
+    println("저는 바보계산기입니다. ")
+    println("첫 번째 숫자를 입력하세요 :  ")
     var num1 = readln().toInt()
     calculator.num1 = num1
     println("연산 기호를 입력하세요: ( +, -, *, / ) ")
